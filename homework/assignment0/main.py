976863f2c967092fc2317105e382f3dcd45ca122
DanielY1783 <daniel.yan@vanderbilt.edu>
2020-01-07 10:06:31 -0600

    Finished commenting main function for assignment 0

--- homework/assignment0/main.py
+++ homework/assignment0/main.py
@@ -1,3 +1,4 @@
+# Imports for Pytorch
 from __future__ import print_function
 import argparse
 import torch
@@ -7,18 +8,36 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
-
+# Define the neural network
 class Net(nn.Module):
+    # Define the dimensions for each layer.
     def __init__(self):
         super(Net, self).__init__()
+        # First convolutional layer has 1 input channel, 32 output channels, a 3x3 square kernel, and a stride of 1.
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        # Second convolutional layer has 32 input channels since the first layer has 32 output channels.
+        # The second layer has 64 output channels, uses a 3x3 square kernel, and has a stride of 1.
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        # Dropout is performed twice in the network, with the first time set to 0.25 and the second time set to 0.5.
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
+        # Two fully connected layers. The input shape to the first fully connected layer is 64x12x12 = 9216. This is
+        # because the MNIST image is 28x28, so the first convolutional layer changes it to 26x26 since the kernel
+        # is 3x3. The second convolutional layer changes it to 24x24. We then have a maxpool layer that changes
+        # the dimensions to 12x12. Since we have 64 channels as the output from the second convolutional layer,
+        # we get a total of 64x12x12 = 9216. The output from the first fully connected layer is 128.
         self.fc1 = nn.Linear(9216, 128)
+        # Second fully connected layer takes in shape of 128 from the output of the first fully connected layer
+        # and then has 10 outputs because we have 10 classes for MNIST.
         self.fc2 = nn.Linear(128, 10)
 
+    # Define the structure for forward propagation.
     def forward(self, x):
+        # We begin with a convolutional layer with a Relu activation function. We then use a second
+        # convolutional layer and perform max pooling and dropout on the output. We then flatten the
+        # 64 channels from the output of the second convolutional layer to pass to the first fully
+        # connected layer, and use a Relu activation function for the output. We then perform dropout
+        # a second time and send the output for the softmax function, since we are performing classification.
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
@@ -68,7 +87,9 @@ def test(args, model, device, test_loader):
 
 
 def main():
-    # Training settings
+    # Command line arguments for hyperparameters of training and testing batch size, the number of
+    # epochs, the learning rate, gamma, and other settings such as whether to use a GPU device, the
+    # random seed, how often to log, and whether we should save the model.
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
@@ -90,13 +111,19 @@ def main():
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
+    # Command to use gpu depending on command line arguments and if there is a cuda device
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
+    # Random seed to use
     torch.manual_seed(args.seed)
 
+    # Set to either use gpu or cpu
     device = torch.device("cuda" if use_cuda else "cpu")
 
+    # GPU keywords.
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    # Load in the training and testing datasets. Convert to
+    # pytorch tensor and normalize.
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
@@ -111,15 +138,20 @@ def main():
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    # Run model on GPU if available
     model = Net().to(device)
+    # Specify Adadelta optimizer
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
+    # Run for the set number of epochs. For each epoch, run the training
+    # and the testing steps.
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
         scheduler.step()
 
+    # Save model if specified by the command line argument
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
 
