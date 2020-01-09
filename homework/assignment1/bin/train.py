8af709315e2155b7d89d83171918021404de6ff5
DanielY1783 <daniel.yan@vanderbilt.edu>
2020-01-08 21:12:14 -0600

    Working network

--- homework/assignment1/bin/train.py
+++ homework/assignment1/bin/train.py
@@ -57,6 +57,7 @@ class ToTensor(object):
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
+        label = label.reshape(-1)
         return {'image': torch.from_numpy(image),
                 'label': torch.from_numpy(label)}
 
@@ -65,58 +66,49 @@ class Net(nn.Module):
     # Define the dimensions for each layer.
     def __init__(self):
         super(Net, self).__init__()
-        # First convolutional layer has 1 input channel, 32 output channels,
+        # First convolutional layer has 3 input channels, 15 output channels,
         # a 3x3 square kernel, and a stride of 1.
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        # Second convolutional layer has 32 input channels
-        # since the first layer has 32 output channels.
-        # The second layer has 64 output channels, uses
-        # a 3x3 square kernel, and has a stride of 1.
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv1 = nn.Conv2d(3, 15, 3, 1)
+        # Second convolutional layer has 30 input channels, 30 output channels,
+        # a 3x3 square kernel, and a stride of 1.
+        self.conv2 = nn.Conv2d(15, 30, 3, 1)
         # Dropout is performed twice in the network,
         # with the first time set to 0.25 and the
         # second time set to 0.5.
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        # Two fully connected layers. The input shape to the
-        # first fully connected layer is 64x12x12 = 9216. This is
-        # because the MNIST image is 28x28, so the first
-        # convolutional layer changes it to 26x26 since the kernel
-        # is 3x3. The second convolutional layer changes it to 24x24.
-        # We then have a maxpool layer that changes
-        # the dimensions to 12x12. Since we have 64 channels as
-        # the output from the second convolutional layer,
-        # we get a total of 64x12x12 = 9216. The output from
-        # the first fully connected layer is 128.
-        self.fc1 = nn.Linear(9216, 128)
-        # Second fully connected layer takes in shape of
-        # 128 from the output of the first fully connected layer
-        # and then has 10 outputs because we have 10 classes for MNIST.
-        self.fc2 = nn.Linear(128, 10)
+        # Two fully connected layers. Input is 2347380 because 243x161x60
+        # as shown in the forward part.
+        self.fc1 = nn.Linear(290400, 128)
+        # Second fully connected layer has 128 inputs and 2 outputs for x and y values
+        self.fc2 = nn.Linear(128, 2)
 
     # Define the structure for forward propagation.
     def forward(self, x):
-        # We begin with a convolutional layer with a
-        # Relu activation function. We then use a second
-        # convolutional layer and perform max pooling
-        # and dropout on the output. We then flatten the
-        # 64 channels from the output of the second
-        # convolutional layer to pass to the first fully
-        # connected layer, and use a Relu activation
-        # function for the output. We then perform dropout
-        # a second time and send the output for the
-        # softmax function, since we are performing classification.
+        # Input dimensions: 490x326x3
+        # Output dimensions: 488x324x15
         x = self.conv1(x)
-        x = F.relu(x)
+        # Input dimensions: 488x324x15
+        # Output dimensions: 244x162x15
+        x = F.max_pool2d(x, 2)
+        # Input dimensions: 244x162x15
+        # Output dimensions: 242x160x15
         x = self.conv2(x)
+        # Input dimensions: 242x160x30
+        # Output dimensions: 121x80x30
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
+        # Input dimensions: 121x80x30
+        # Output dimensions: 290400x1
         x = torch.flatten(x, 1)
+        # Input dimensions: 290400x1
+        # Output dimensions: 128x1
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
+        # Input dimensions: 128x1
+        # Output dimensions: 2x1
+        output = self.fc2(x)
         return output
 
 
@@ -124,15 +116,15 @@ def train(args, model, device, train_loader, optimizer, epoch):
     # Specify that we are in training phase
     model.train()
     # Iterate through all minibatches.
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, batch_sample in enumerate(train_loader):
         # Send training data and the training labels to GPU/CPU
-        data, target = data.to(device), target.to(device)
+        data, target = batch_sample["image"].to(device, dtype=torch.float32), batch_sample["label"].to(device, dtype=torch.float32)
         # Zero the gradients carried over from previous step
         optimizer.zero_grad()
         # Obtain the predictions from forward propagation
         output = model(data)
-        # Compute the negative log likelihood of the loss function
-        loss = F.nll_loss(output, target)
+        # Compute the mean squared error for loss
+        loss = F.mse_loss(output, target)
         # Perform backward propagation to compute the negative gradient, and
         # update the gradients with optimizer.step()
         loss.backward()
@@ -147,9 +139,8 @@ def train(args, model, device, train_loader, optimizer, epoch):
 def test(args, model, device, test_loader):
     # Specify that we are in evaluation phase
     model.eval()
-    # Set the loss and number of correct instances initially to 0.
+    # Set the loss initially to 0.
     test_loss = 0
-    correct = 0
     # No gradient calculation because we are in testing phase.
     with torch.no_grad():
         # For each testing example, we run forward
@@ -157,20 +148,19 @@ def test(args, model, device, test_loader):
         # testing prediction. Update the total loss
         # and the number of correct predictions
         # with the counters from above.
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+        for batch_idx, batch_sample in enumerate(test_loader):
+            # Send training data and the training labels to GPU/CPU
+            data, target = batch_sample["image"].to(device, dtype=torch.float32), batch_sample["label"].to(device,
+                                                                                                           dtype=torch.float32)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            test_loss += F.mse_loss(output, target).item()
 
     # Average the loss by dividing by the total number of testing instances.
     test_loss /= len(test_loader.dataset)
 
     # Print out the statistics for the testing set.
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print('\nTest set: Average loss: {:.4f}\n'.format(
+        test_loss))
 
 
 def main():
@@ -213,9 +203,9 @@ def main():
     # GPU keywords.
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     # Load in the training and testing datasets. Convert to pytorch tensor.
-    train_data = DetectionImages("../data/labels/train_labels.txt", "../data/train", transform=ToTensor())
+    train_data = DetectionImages(csv_file="../data/labels/train_labels.txt", root_dir="../data/train", transform=ToTensor())
     train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
-    test_data = DetectionImages("../data/labels/validation_labels.txt", "../data/validation", transform=ToTensor())
+    test_data = DetectionImages(csv_file="../data/labels/validation_labels.txt", root_dir="../data/validation", transform=ToTensor())
     test_loader = DataLoader(test_data, batch_size=args.test_batch_size, shuffle=True, num_workers=0)
 
     # Run model on GPU if available
