96afcb2ae054fc9b55823e65d6622b8625a7a958
DanielY1783 <daniel.yan@vanderbilt.edu>
2020-01-23 22:16:12 -0600

    Working window based classification

--- homework/assignment1/bin/test.py
+++ homework/assignment1/bin/test.py
@@ -12,17 +12,11 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from skimage import io, transform
 
-loss = nn.L1Loss(reduction="sum")
-loss2 = nn.L1Loss(reduction="none")
-input = torch.randn(3, 5)
-target = torch.randn(3, 5)
+loss = nn.CrossEntropyLoss()
+input = torch.randn(3, 5, requires_grad=True)
+target = torch.empty(3, dtype=torch.long).random_(5)
 output = loss(input, target)
-output2 = loss2(input, target)
+output.backward()
 print(input)
 print(target)
 print(output)
-print(output2)
-# print(input.size())
-# print(target.size())
-# print(output.size())
-# print(output2.size())
